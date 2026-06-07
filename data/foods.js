bc169e3c0bcf91fe418024f83627af82b3f717a6
dar12378 <danielitzkovich@outlook.co.il>
2026-06-07 18:08:03 +0300

    Add FOOD_DB with nutritional information for foods

---
+++ data/foods.js
@@ -0,0 +1,34 @@
+export const FOOD_DB = {
+  "סנדוויץ": {
+    calories: 420,
+    protein: 18,
+    carbs: 46,
+    fat: 17,
+    sugar: 5,
+    fiber: 4,
+    sodium: 690,
+    cholesterol: 85
+  },
+
+  "פלפל ממולא": {
+    calories: 360,
+    protein: 16,
+    carbs: 42,
+    fat: 13,
+    sugar: 8,
+    fiber: 6,
+    sodium: 740,
+    cholesterol: 55
+  },
+
+  "פסטה": {
+    calories: 560,
+    protein: 19,
+    carbs: 78,
+    fat: 19,
+    sugar: 7,
+    fiber: 5,
+    sodium: 620,
+    cholesterol: 45
+  }
+};
